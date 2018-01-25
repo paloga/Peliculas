e0c9f492b1e93f452f87f1996593b504f135e91f
Pablo <pabete.l@gmail.com>
2018-01-25 08:31:08 +0100

    Subimos log out, loggin, registro

--- src/PeliculasBundle/Controller/DefaultController.php
+++ src/PeliculasBundle/Controller/DefaultController.php
@@ -5,7 +5,9 @@ namespace PeliculasBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use PeliculasBundle\Entity\Pelicula;
+use PeliculasBundle\Entity\User;
 use PeliculasBundle\Form\PeliculaType;
+use PeliculasBundle\Form\UserType;
 use PeliculasBundle\Entity\Genero;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -24,9 +26,34 @@ class DefaultController extends Controller
     /**
      * @Route("/registro", name="registro")
      */
-    public function registroAction()
+    public function registroAction(Request $request)
     {
-        return $this->render('PeliculasBundle:Default:registro.html.twig');
+      // 1) build the form
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // 3) Encode the password (you could also do this via Doctrine listener)
+            $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+            $roles = ["ROLE_USER"];
+            $user->setRoles($roles);
+
+
+            // 4) save the User!
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            // ... do any other work - like sending them an email, etc
+            // maybe set a "flash" success message for the user
+
+            return $this->redirectToRoute('home');
+        }
+        return $this->render('PeliculasBundle:Default:registro.html.twig', array('form' => $form -> createView()));
     }
     /**
     * @Route("/usuarios/login", name="login")
